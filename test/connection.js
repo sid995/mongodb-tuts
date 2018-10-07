be1e6f2b36c5abd51f13920549382d196edd9ee1
Siddharth Kundu <sk31395@gmail.com>
2018-10-07 19:46:54 +0530

    ES6 promises and re-arranging code to correct format

--- test/connection.js
+++ test/connection.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 
-// Connect to mongodb
-mongoose.connect("mongodb://localhost/testdb");
+// ES6 Promises
+mongoose.Promise = global.Promise;
 
-mongoose.connection
-  .once("open", function() {
-    console.log("Connection Created");
-  })
-  .on("error", function(error) {
-    console.log("Connection error ", error);
-  });
+// Connect to the db before test run
+before(function(done) {
+  // Connect to mongodb
+  mongoose.connect("mongodb://localhost/testdb");
+
+  mongoose.connection
+    .once("open", function() {
+      console.log("Connection Created");
+      done();
+    })
+    .on("error", function(error) {
+      console.log("Connection error ", error);
+    });
+});
